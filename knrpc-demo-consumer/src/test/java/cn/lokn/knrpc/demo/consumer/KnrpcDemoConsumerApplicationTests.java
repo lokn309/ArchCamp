4dfc340c1d9ced67723ad5e6f02158c46f69b600
lokn309 <1163141991@qq.com>
2024-03-25 00:13:05 +0800

    add consumer test

--- knrpc-demo-consumer/src/test/java/cn/lokn/knrpc/demo/consumer/KnrpcDemoConsumerApplicationTests.java
+++ knrpc-demo-consumer/src/test/java/cn/lokn/knrpc/demo/consumer/KnrpcDemoConsumerApplicationTests.java
@@ -1,13 +1,34 @@
 package cn.lokn.knrpc.demo.consumer;
 
+import cn.lokn.knrpc.demo.provider.KnrpcDemoProviderApplication;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.swing.*;
 
 @SpringBootTest
 class KnrpcDemoConsumerApplicationTests {
 
+    static ApplicationContext context;
+
+    @BeforeAll
+    static void init() {
+        context = SpringApplication.run(KnrpcDemoProviderApplication.class,
+                "--server.port=8091");
+    }
+
     @Test
     void contextLoads() {
+    }
+
+    @AfterAll
+    static void destroy() {
+        SpringApplication.exit(context, () -> 1);
     }
 
 }
