668827713f83f2a3c9cb7ea25476d00c67ff3e86
lokn309 <1163141991@qq.com>
2024-04-11 00:04:52 +0800

    add ConsumerProperties

---
+++ knrpc-core/src/main/java/cn/lokn/knrpc/core/config/ConsumerProperties.java
@@ -0,0 +1,24 @@
+package cn.lokn.knrpc.core.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @description: 消费者配置类
+ * @author: lokn
+ * @date: 2024/04/10 23:51
+ */
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "knrpc.consumer")
+public class ConsumerProperties {
+
+    private int retries = 1;
+    private int timeout = 1000;
+    private int faultLimit = 10;
+    private int halfOpenInitialDelay = 10_000;
+    private int halfOpenDelay = 60_000;
+    private int grayRatio = 0;
+
+}
